ff8c27e1a6b6762d0253c8f660986fe010acfd73
jferrin1996 <jakeferrin1996@gmail.com>
2020-11-12 10:28:33 -0600

    initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\14022\documents\college\masters\fall_2020\full_stack_development\a4\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
